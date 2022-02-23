222c93be55a00b3f5414c704172e2bd102370e58
Farai-Mashiri <f.mashiri.mashiri@gmail.com>
2022-02-23 10:26:14 +0200

    Added csv file to read data for the periodic table

--- interactive_periodic_table.py
+++ interactive_periodic_table.py
@@ -1,3 +1,11 @@
-# This is a sample Python script.
-# A Small interactive periodic table program in python
-# This project is adapted from a project
+"""This is a sample Python script. A Small interactive periodic table program in python
+This project is adapted from a project by Al Sweigart
+This project uses a csv file in order to manipulate data and sort it as a visual periodic table"""
+
+# Data from https://en.wikipedia.org/wiki/List_of_chemical_elements
+# Highlight the table, copy it, then paste it into a spreadsheet program
+# like Excel or Google Sheets like in https://invpy.com/elements
+# Then save this file as periodictable.csv.
+# Or download this csv file from https://invpy.com/periodictable.csv
+
+#Read csv file
